01877067ead3e1cdda13b19391c53d7a78009df8
SvenKeimpema <keimpje1234@gmail.com>
2023-05-19 15:51:03 +0200

    test: added some more tests :)

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--host", action="store", default="default name")
+
+
+@pytest.fixture(scope='session')
+def host(request):
+    host_value = request.config.option.host
+    if host_value is None:
+        print("skipping.. no given host param")
+        pytest.skip()
+    return host_value
