9533a4abbffac3693e2948cdb3f6621d995336e7
SvenKeimpema <keimpje1234@gmail.com>
2023-05-19 16:00:37 +0200

    tests: commit bug?

--- tests/sshfs_tests.py
+++ tests/sshfs_tests.py
@@ -5,16 +5,19 @@ import sys
 sys.path.append("..")
 from src.edwh_sshfs_plugin import fabfile
 sys.path.append("tests")
-import unittest
+import pytest
 import socket
 
 
-class TestYourModule(unittest.TestCase):
-    def test_is_port_open(self):
-        self.assertTrue(fabfile.get_available_port() == fabfile.get_available_port())
-        s = socket.socket()
-        open_port = fabfile.get_available_port()
-        s.bind(("127.0.0.1", open_port))
-        s.listen(5)
-        self.assertFalse(open_port == fabfile.get_available_port())
-        s.close()
+def test_is_port_open():
+    assert fabfile.get_available_port() == fabfile.get_available_port()
+    s = socket.socket()
+    open_port = fabfile.get_available_port()
+    s.bind(("127.0.0.1", open_port))
+    s.listen(5)
+    assert open_port != fabfile.get_available_port()
+    s.close()
+
+
+def test_is_param_correct(host):
+    print(host)
