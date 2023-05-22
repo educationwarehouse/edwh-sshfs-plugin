a362d369ca86bbd921829ea0b66e539304e64e39
SvenKeimpema <keimpje1234@gmail.com>
2023-05-22 14:10:28 +0200

    docs: updated fabfile.py docs

--- src/edwh_sshfs_plugin/fabfile.py
+++ src/edwh_sshfs_plugin/fabfile.py
@@ -14,6 +14,15 @@ from fabric import task
 STOP_RUNNING = False
 
 def get_available_port(c):
+    """
+    Retrieves an available port that is both locally and remotely accessible.
+
+    Args:
+        c: The connection object used to communicate with the remote server.
+
+    Returns:
+        int: An available port number that is both locally and remotely accessible.
+    """
     # get remote available ports
     remote_ports = get_remote_available_ports(c)
 
@@ -24,6 +33,16 @@ def get_available_port(c):
 
 
 def get_remote_available_ports(c):
+    """
+        Retrieves the list of available ports on the remote server.
+
+        Args:
+            c: The connection object used to communicate with the remote server.
+
+        Returns:
+            list: A list of available port numbers on the remote server.
+        """
+
     port_cmd = c.run("nc -z -v 127.0.0.1 2220-2300 2>&1 | grep refused | cut -d ' ' -f 6", hide=True)
     # return available_port
     return port_cmd.stdout.split("\n")
@@ -31,6 +50,16 @@ def get_remote_available_ports(c):
 
 # check if there are any available ports
 def get_local_available_port(start_port=2222):
+    """
+       Retrieves a list of available ports on the local machine.
+
+       Args:
+           start_port (optional): The starting port number to search for available ports.
+               Defaults to 2222.
+
+       Returns:
+           list: A list of available port numbers on the local machine.
+       """
     available_ports = []
 
     # check all ports between start_port and 60000
@@ -48,8 +77,15 @@ def get_local_available_port(start_port=2222):
     return available_ports
 
 
-@task
+@task(help={'dir' "The directory path to be unmounted."})
 def unmount_dir(c, dir):
+    """
+        Unmounts a directory on the server.
+
+        Returns:
+            None
+        """
+
     umount_server_dir = f"umount {dir}"
     print(f"running {umount_server_dir}")
     if not c.run(umount_server_dir, warn=True).ok:
@@ -61,8 +97,18 @@ def unmount_dir(c, dir):
         print("successfully exited :)")
 
 
-@task()
+@task(help={'workstation-dir' : "The directory path on the local machine to mount the server directory.",
+            'server-dir' : "The directory path on the remote server to be mounted.",
+            'queue' : "An optional queue object used for synchronization. Defaults to None, optional"})
 async def remote_mount(c, workstation_dir, server_dir, queue=None):
+    """
+    The remote_mount function is an asynchronous Python task that allows you to mount a remote directory
+    on your local machine using SSHFS (SSH Filesystem). It establishes a secure connection to a remote server,
+    forwards a port, and mounts the remote directory on the local machine.
+
+    Returns:
+        None
+    """
     if not hasattr(c, "host"):
         print("please give up a host using -H")
         exit(255)
@@ -84,8 +130,18 @@ async def remote_mount(c, workstation_dir, server_dir, queue=None):
         unmount_dir(c, server_dir)
 
 
-@task()
+@task(help={'workstation-dir' 'The directory path on the local machine to be mounted.',
+            'server-dir' 'The directory path on the remote server to mount the workstation directory.',
+            'queue' 'An optional queue object used for synchronization. Defaults to None, optional'})
 async def local_mount(c, workstation_dir, server_dir, queue=None):
+    """
+        The local_mount function is a Python task that enables the mounting of a remote directory on a local workstation
+        using SSHFS (SSH Filesystem). It establishes a secure connection to a remote server and mounts a specified
+        directory from the server onto a local directory on the workstation.
+
+        Returns:
+            None
+        """
     os.popen(f"lsof -n {workstation_dir} 2>/dev/null")
     if not hasattr(c, "host"):
         print("please give up a host using -H")
