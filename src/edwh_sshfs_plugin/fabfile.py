3a451176a7cffaa6af51b052436a34644c79d831
Robin van der Noord <robinvandernoord@gmail.com>
2024-04-12 12:48:47 +0200

    feat: add `ensure-sshfs` subcommand to make sure sshfs is installed when required + use new `require_sudo` for better sudo prompting

--- src/edwh_sshfs_plugin/fabfile.py
+++ src/edwh_sshfs_plugin/fabfile.py
@@ -1,28 +1,30 @@
+import asyncio
+import getpass
 import os
 import socket
-import getpass
-import warnings
 
-from invoke import task, context
-from plumbum import BG
-from plumbum.commands.processes import CommandNotFound
-
-# import anyio
-import asyncio
-
-try:
-    from plumbum.cmd import ssh, sshfs
-except (ImportError, CommandNotFound):
-    warnings.warn(
-        "edwh sshfs plugin is installed but sshfs is missing. "
-        "Please check README.md#installation on how to fix this! "
-        "The commands in this plugin will NOT work unless this is fixed."
-    )
-
-STOP_RUNNING = False
+import edwh.tasks
+from invoke import Context, context, task
+from plumbum import BG, local
 
 
-def get_available_port(c):
+def get_remote_available_ports(c: Context):
+    """
+    Retrieves the list of available ports on the remote server.
+
+    Args:
+        c: The connection object used to communicate with the remote server.
+
+    Returns:
+        list: A list of available port numbers on the remote server.
+    """
+
+    port_cmd = c.run("nc -z -v 127.0.0.1 2220-2300 2>&1 | grep refused | cut -d ' ' -f 6", hide=True)
+    # return available_port
+    return port_cmd.stdout.split("\n")
+
+
+def get_available_port(c: Context):
     """
     Retrieves an available port that is both locally and remotely accessible.
 
@@ -41,24 +43,8 @@ def get_available_port(c):
             return local_port
 
 
-def get_remote_available_ports(c):
-    """
-    Retrieves the list of available ports on the remote server.
-
-    Args:
-        c: The connection object used to communicate with the remote server.
-
-    Returns:
-        list: A list of available port numbers on the remote server.
-    """
-
-    port_cmd = c.run("nc -z -v 127.0.0.1 2220-2300 2>&1 | grep refused | cut -d ' ' -f 6", hide=True)
-    # return available_port
-    return port_cmd.stdout.split("\n")
-
-
 # check if there are any available ports
-def get_local_available_port(start_port=2222):
+def get_local_available_port(start_port: int = 2222):
     """
     Retrieves a list of available ports on the local machine.
 
@@ -77,25 +63,38 @@ def get_local_available_port(start_port=2222):
         s.settimeout(0.5)
         try:
             s.connect(("127.0.0.1", port))
-            s.close()
             continue
-        except:
-            s.close()
+        except Exception:
             available_ports.append(str(port))
+        finally:
+            s.close()
 
     return available_ports
 
 
-def getuid():
-    return os.getuid()
+@task()
+def setup(c: Context):
+    """
+    Installs the necessary packages for SSHFS.
+    """
+    if edwh.tasks.require_sudo(c):
+        c.sudo("apt update")
+        c.sudo("apt install -y openssh-client openssh-server sshfs")
+        c.sudo("systemctl enable ssh")
+        c.sudo("ufw allow ssh")
 
 
-def getgid():
-    return os.getgid()
+@task()
+def ensure_sshfs(c: Context):
+    """
+    Ensures that SSHFS is installed on the local machine.
+    """
+    if not c.run("which sshfs", hide=True, warn=True).ok:
+        setup(c)
 
 
-@task(help={'dir': "The directory path to be unmounted."})
-def unmount_dir(c, dir):
+@task(help={'folder': "The directory path to be unmounted."}, pre=[ensure_sshfs])
+def unmount_dir(c, folder):
     """
     Unmounts a directory on the server.
 
@@ -103,15 +102,15 @@ def unmount_dir(c, dir):
         None
     """
 
-    umount_server_dir = f"umount {dir}"
+    umount_server_dir = f"umount {folder}"
     print(f"running {umount_server_dir}")
     if not c.run(umount_server_dir, warn=True).ok:
         print("cannot unmount because following processes are still running and using the mount")
-        c.run(f"lsof -n {dir} 2>/dev/null")
+        c.run(f"lsof -n {folder} 2>/dev/null")
         pids = c.run("lsof -t -n /home/ubuntu/testing 2>/dev/null", hide=True).stdout.strip().split('\n')
-        print(f'Terminate with: kill {" ".join(pids)}; sleep 2; umount {dir}')
+        print(f'Terminate with: kill {" ".join(pids)}; sleep 2; umount {folder}')
     else:
-        print("successfully exited :)")
+        print("suFccessfully exited :)")
 
 
 @task(
@@ -119,9 +118,15 @@ def unmount_dir(c, dir):
         'workstation-dir': "The directory path on the local machine to mount the server directory.",
         'server-dir': "The directory path on the remote server to be mounted.",
         'queue': "An optional queue object used for synchronization. Defaults to None, optional",
-    }
+    },
+    pre=[ensure_sshfs],
 )
-def remote_mount(c, workstation_dir, server_dir, queue=None):
+def remote_mount(
+    c,
+    workstation_dir,
+    server_dir,
+    queue=None,
+):
     """
     The remote_mount function is an asynchronous Python task that allows you to mount a remote directory
     on your local machine using SSHFS (SSH Filesystem). It establishes a secure connection to a remote server,
@@ -131,19 +136,19 @@ def remote_mount(c, workstation_dir, server_dir, queue=None):
         None
     """
     if not hasattr(c, "host"):
-        print("please give up a host using -H")
+        print("please provide a host using -H")
         exit(255)
 
     # get an available port that is usable on local and remote so we can setup a connection with the ports
     port = get_available_port(c)
-    ssh_cmd = ssh["-A", f"-R {port}:127.0.0.1:22", f"{c.user}@{c.host}"]
+    ssh_cmd = local['ssh']["-A", f"-R {port}:127.0.0.1:22", f"{c.user}@{c.host}"]
     sshfs_cmd = ssh_cmd[
         "sshfs",
         "-f",
         f"-p {port}",
         "-o allow_root,default_permissions",
         "-o StrictHostKeyChecking=no,reconnect,ServerAliveInterval=3,ServerAliveCountMax=3,"
-        f"uid={getuid()},gid={getgid()}",
+        f"uid={os.getuid()},gid={os.getgid()}",
         f"{getpass.getuser()}@127.0.0.1:{workstation_dir}",
         f"{server_dir}",
     ]
@@ -161,7 +166,8 @@ def remote_mount(c, workstation_dir, server_dir, queue=None):
         'workstation-dir': 'The directory path on the local machine to be mounted.',
         'server-dir': 'The directory path on the remote server to mount the workstation directory.',
         'queue': 'An optional queue object used for synchronization. Defaults to None, optional',
-    }
+    },
+    pre=[ensure_sshfs],
 )
 def local_mount(c, workstation_dir, server_dir, queue=None):
     """
@@ -174,13 +180,15 @@ def local_mount(c, workstation_dir, server_dir, queue=None):
     """
     # os.popen(f"lsof -n {workstation_dir} 2>/dev/null")
     if not hasattr(c, "host"):
-        print("please give up a host using -H")
+        print("please provide a host using -H")
         exit(255)
+
     # TODO: remove mount on exit
-    sshfs_cmd = sshfs[
+    sshfs_cmd = local['sshfs'][
         "-f",
         "-o",
-        "allow_root,default_permissions,umask=000,StrictHostKeyChecking=no,reconnect," f"uid={getuid()},gid={getgid()}",
+        "allow_root,default_permissions,umask=000,StrictHostKeyChecking=no,reconnect,"
+        f"uid={os.getuid()},gid={os.getgid()}",
         f"{c.user}@{c.host}:{server_dir}",
         workstation_dir,
     ]
@@ -204,13 +212,14 @@ async def async_local_mount(c, workstation_dir, server_dir, event=None):
     async version of local_mount
     """
     if not hasattr(c, "host"):
-        print("please give up a host using -H")
+        print("please provide a host using -H")
         exit(255)
 
-    sshfs_cmd = sshfs[
+    sshfs_cmd = local['sshfs'][
         "-f",
         "-o",
-        "allow_root,default_permissions,umask=000,StrictHostKeyChecking=no,reconnect," f"uid={getuid()},gid={getgid()}",
+        "allow_root,default_permissions,umask=000,StrictHostKeyChecking=no,reconnect,"
+        f"uid={os.getuid()},gid={os.getgid()}",
         f"{c.user}@{c.host}:{server_dir}",
         workstation_dir,
     ]
@@ -235,19 +244,19 @@ async def async_remote_mount(c, workstation_dir, server_dir, event=None):
     async version of remote_mount
     """
     if not hasattr(c, "host"):
-        print("please give up a host using -H")
+        print("please provide a host using -H")
         exit(255)
 
     # get an available port that is usable on local and remote so we can setup a connection with the ports
     port = get_available_port(c)
-    ssh_cmd = ssh["-A", f"-R {port}:127.0.0.1:22", f"{c.user}@{c.host}"]
+    ssh_cmd = local['ssh']["-A", f"-R {port}:127.0.0.1:22", f"{c.user}@{c.host}"]
     sshfs_cmd = ssh_cmd[
         "sshfs",
         "-f",
         f"-p {port}",
         "-o allow_root,default_permissions",
         "-o StrictHostKeyChecking=no,reconnect,ServerAliveInterval=3,ServerAliveCountMax=3,"
-        f"uid={getuid()},gid={getgid()}",
+        f"uid={os.getuid()},gid={os.getgid()}",
         f"{getpass.getuser()}@127.0.0.1:{workstation_dir}",
         f"{server_dir}",
     ]
